6c2c500ddb0509d51effee64f1429a161d35c755
anonymous-user31 <150164357+anonymous-user31@users.noreply.github.com>
2023-12-08 04:48:30 +0000

    Update random_walk.R

--- question-4-code/random_walk.R
+++ question-4-code/random_walk.R
@@ -53,3 +53,20 @@ plot2 <- ggplot(aes(x = x, y = y), data = data2) +
   ylab("y-coordinate")
 
 grid.arrange(plot1, plot2, ncol=2)
+
+
+## Answers: 
+
+## Observations:
+I see that the provided code simulates two random walks, each with 500 steps. The random walks start at the origin (0, 0) and move in random directions with step sizes determined by a random angle.
+
+The two random walks appear as paths with different trajectories.
+Each step is represented by a point on the plot, and the color of the points indicates the time step.
+
+Random seeds: 
+A random seed is a starting point for generating random numbers using a pseudorandom number generator (PRNG). PRNGs are deterministic algorithms that produce sequences of numbers that appear random but are reproducible if you start with the same seed. In R, you can set a random seed using the set.seed() function. Setting a random seed ensures that you get the same sequence of random numbers every time you run your code, making your simulations or analyses reproducible.
+
+To make the simulation of Brownian motion reproducible, you can set a random seed before generating random numbers using functions like runif() or rnorm(). This ensures that if you use the same seed in the future, you'll get the same sequence of random values, making your simulations consistent.
+
+
+
